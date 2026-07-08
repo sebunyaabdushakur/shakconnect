b87905c0df362bb235d2c16b3e25609b9b15bc2b
Bigshakur <bigshakur@example.com>
2026-07-08 09:30:42 +0300

    Add PostgreSQL database config

--- shakconnect/settings.py
+++ shakconnect/settings.py
@@ -59,12 +59,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shakconnect.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
