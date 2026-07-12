d06893e52370cff7bda047baeb52824a096845d1
Bigshakur <bigshakur@example.com>
2026-07-13 00:21:11 +0300

    Fix settings.py import error

--- shakconnect/settings.py
+++ shakconnect/settings.py
@@ -2,7 +2,7 @@ from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework_simplejwt.tokens import RefreshToken
+
 from django.contrib.auth import authenticate
 from django.conf import settings
 from .models import Student
